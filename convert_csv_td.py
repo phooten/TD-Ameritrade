cf0f6ea628a87c7d0f5d8e8699eb4fc0e62863dc
phooten <parkerhooten@gmail.com>
2022-10-15 23:59:00 -0600

    start of utility tool

---
+++ convert_csv_td.py
@@ -0,0 +1,16 @@
+#!/bin/
+
+
+
+
+# Overview:
+#	
+#	
+#
+#
+#	Goals of this script:
+#		- convert the .csv of option trades from TD trade history to readable format
+#		- show data in some sort of QT format. Maybe display the data and allow user customization?
+
+main:
+
