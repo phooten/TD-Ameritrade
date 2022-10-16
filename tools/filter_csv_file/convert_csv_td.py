c39a9eab801dd9d66612f660636f9f3fe627de24
phooten <parkerhooten@gmail.com>
2022-10-16 00:46:44 -0600

    created an outline / file system for tools

--- tools/filter_csv_file/convert_csv_td.py
+++ tools/filter_csv_file/convert_csv_td.py
@@ -12,5 +12,12 @@
 #		- convert the .csv of option trades from TD trade history to readable format
 #		- show data in some sort of QT format. Maybe display the data and allow user customization?
 
+
+# Outline / Plan:
+#	- take in newest input file, OR argument file
+#	- ensure argument exists if applicable
+#	- filter through excel, and append to a new file
+#	- 
+
 main:
 
