6dd8b3a843ebe1e08ea5c77960ea9d66c1fec996
phooten <parkerhooten@gmail.com>
2022-11-02 09:12:44 -0600

    starting file to analyze the ticker and plays from the csv

---
+++ tools/analyze_csv.py
@@ -0,0 +1,28 @@
+#!/bin/
+
+# Overview:
+#   Goals of this script:
+#       - Take in information from from a csv file and output break-even values
+#         for a particular stock
+
+# Outline / Plan:
+#   - First glance, it might be good to make an object for each ticker that 
+#     contains all the information about its purchase price, break even price,
+#     how many options were used, average option price, etc. 
+
+
+# Libraries
+import csv
+import pandas as pd
+import os
+
+
+def main():
+    ticker_list = []
+    ticker_baseprice = []
+    ticker_otions = []
+    ticker_breakeven = []
+
+
+if __name__ == "__main__":
+    main()
