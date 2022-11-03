7967bf5d86eb6cbfbb81ad5bf166ca58df406358
phooten <parkerhooten@gmail.com>
2022-11-03 00:14:14 -0600

    starting to make use of file. Ability to bring in the file we want to analyze.

--- tools/analyze_csv.py
+++ tools/analyze_csv.py
@@ -13,8 +13,43 @@
 
 # Libraries
 import csv
-import pandas as pd
 import os
+import pandas as pd
+import sys
+
+
+# TODO: See other python script. Add this to global library
+global_error = "Error: "
+
+
+def analyzeCsv( pPath ):
+    # Check if the file exists
+    print( "Dose " + pPath + " exist?" )
+    if os.path.exists( pPath ):
+        print( "Yes." )
+    else:
+        print( "No.")
+    print()
+    
+    # If file exists, then figure out what it does
+    df = pd.read_csv( pPath, sep=',' )
+    # len_row, len_col = df.shape
+    # print( len_row )
+    # print ( len_col )
+
+    df.head()
+    # for row in df:
+    #     print( row )
+    # for row in range( len_row ):
+        # print( df.loc[ row ] ) # This prints in a block form, not what you want
+
+
+
+def calc_total_commision():
+    # Variables
+    commision = 0
+
+    # Cycle through every row:
 
 
 def main():
@@ -22,6 +57,27 @@ def main():
     ticker_baseprice = []
     ticker_otions = []
     ticker_breakeven = []
+
+    # TODO: if no input, find the most recent csv
+    # TODO: Could make this into a function. Things could be more complicated with the year
+    
+    # Selects the path to csv to analyze
+    if len( sys.argv ) == 2:
+        csv_input_path = sys.argv[ 1 ]
+    
+    elif len( sys.argv ) > 2:
+        csv_input_path = global_error + "More than 2 arguments"
+        return -1
+        
+    else:
+        # TODO: This file name is the same as in convert_csv_td.py
+        # relative_path =
+        # csv_input_name = 'output.csv'
+        csv_input_name = 'testing.csv'
+        csv_input_path = 'filter_csv_file/output/' + csv_input_name
+        
+    analyzeCsv( csv_input_path )
+    
 
 
 if __name__ == "__main__":
