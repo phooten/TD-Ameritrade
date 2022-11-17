fd74278724f109fa7f44459504c5d1e785a6b228
phooten <parkerhooten@gmail.com>
2022-11-17 00:20:36 -0700

    new gui set up. Desktop version didn't work so I had to make it a gui application. Having issues linking the header file.

---
+++ gui/build-startGui-Qt_6_4_1_for_macOS-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/phoot/Qt/Qt Creator.app/Contents/Resources/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/phoot/Qt/6.4.1/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/phoot/Qt/6.4.1/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
